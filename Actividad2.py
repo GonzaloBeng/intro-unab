380037db7725bec1b59df556e3c3f6f639da5432
Gonzalo Beng <gonzalobeng@gmail.com>
2021-10-21 13:49:52 -0300

    Actividades

--- Actividad2.py
+++ Actividad2.py
@@ -6,7 +6,7 @@ contador = 0
 alumnosPublica = 0
 alumnosPrivada = 0
 
-while (True):
+""" while (True):
     opcion = input('''\n Que tipo de Universidad concurres? Ingresa el numero correspondiente
     1. Universidad publica
     2. Universidad Privada
@@ -24,4 +24,13 @@ while (True):
         print('El porcentaje de alumnos de Univ. Publica es de ', int(((alumnosPublica*100)/contador)),'%')
         print('El porcentaje de alumnos de Univ. Privada es de ', int(((alumnosPrivada*100)/contador)),'%')
     else:
-        print('Ingrese una opcion valida')
+        print('Ingrese una opcion valida') """
+
+
+AlumPublica = int(input('Ingrese la cantidad de alumnos de Univ. Publica '))
+AlumPrivada = int(input('Ingrese la cantidad de alumnos de Univ. Privada '))
+TotalAlumnos = AlumPublica + AlumPrivada
+
+print('La cantidad total de alumnos es de ', TotalAlumnos)
+print('El porcentaje de alumnos de Univ. Publica es de ', int(((AlumPublica*100)/TotalAlumnos)),'%')
+print('El porcentaje de alumnos de Univ. Privada es de ', int(((AlumPrivada*100)/TotalAlumnos)),'%')
