f11b5d03bd97e93de6a44379ca7196458d8d1222
Gonzalo Beng <gonzalobeng@gmail.com>
2021-10-26 18:21:54 -0300

    Actividades

--- Unidad_4/Actividad_5.py
+++ Unidad_4/Actividad_5.py
@@ -8,7 +8,7 @@ encuestados = 0
 candA = 0
 candB = 0
 
-while encuestados < 100:
+""" while encuestados < 100:
     opcion = input('''\n Que candidato va a votar? Ingresa el numero correspondiente
     1. Candidato A
     2. Candidato B
@@ -24,4 +24,21 @@ while encuestados < 100:
 
 
 print('El porcentaje del candidato A es de ', int(((candA*100)/encuestados)),'%')
-print('El porcentaje del candidato B es de ', int(((candB*100)/encuestados)),'%')
+print('El porcentaje del candidato B es de ', int(((candB*100)/encuestados)),'%')"""
+
+
+
+for encuestados in range(0,100):
+    opcion = input('''\n Que candidato va a votar? Ingresa el numero correspondiente
+    1. Candidato A
+    2. Candidato B
+    ''')
+    if opcion == '1':
+        candA = candA + 1
+    elif opcion == '2':
+        candB = candB + 1
+    else:
+        print('Ingrese una opcion valida') 
+
+print('El porcentaje del candidato A es de ', int(((candA*100)/100)),'%')
+print('El porcentaje del candidato B es de ', int(((candB*100)/100)),'%')
