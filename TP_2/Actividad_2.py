6446debc01b27b687e93006290ae36cf89ae7bb8
Gonzalo Beng <gonzalobeng@gmail.com>
2021-10-27 12:15:29 -0300

    Actividades

--- TP_2/Actividad_2.py
+++ TP_2/Actividad_2.py
@@ -4,14 +4,24 @@
 números positivos indefinida (la cantidad que ingresará no se conoce y
 puede cambiar en cada ejecución), finalizando cuando ingresa el número
 0 (que no se tendrá en cuenta). Una vez terminada la lectura de números,
-informar cuál fue el mayor de losnúmeros ingresados. """
+informar cuál fue el mayor de los números ingresados. """
 
 mayor = 0
+totalValores = 0
+totalPares = 0
+totalImpares = 0
 
 while mayor >= 0:
-    num = int(input('Ingrese un numero mayor a 0 '))
+    num = int(input('Ingrese un numero ')) 
     if num > mayor:
         mayor = num
+        totalValores = totalValores + 1
+        if num % 2:
+            totalImpares = totalImpares + 1
+        else:
+            totalPares = totalPares + 1
     elif num == 0:
         print('El mayor numero ingresado es ', mayor)
+        print('La cantidad de valores ingresados es ', totalValores)
+        print('La cantidad de numeros pares es de ', totalPares, 'y de impares', totalImpares)
         break
--- TP_2/Actividad_3.py
+++ TP_2/Actividad_3.py
@@ -11,16 +11,31 @@ aprobados: y” (donde y es la calificación promedio, sólo de los alumnos apro
 opcion = str(input('Analizar calificaciones? S para si: '))
 alumAprob = 0
 contador = 0
-acumNotas = 0
+notasAprob = 0
 promAprob = 0
+mayor = 0
+notasDesaprob = 0
+alumDesaprob = 0
+promDesaprob = 0
 
 while opcion == 'S':
     nota = int(input('Calificacion del alumno: '))
-    if nota >= 4:
-            acumNotas = acumNotas + nota
+    if nota >= 4 and nota <= 10:
+            notasAprob = notasAprob + nota
             alumAprob = alumAprob + 1
             promAprob = promAprob + 1
-    contador = contador + 1
+            contador = contador + 1
+            if nota > mayor:
+                mayor = nota
+    elif nota > 10:
+        print('Ingrese una nota valida')
+    else:
+        notasDesaprob = notasDesaprob + nota
+        alumDesaprob = alumDesaprob + 1
+        promDesaprob = promDesaprob + 1
+        contador = contador + 1
     opcion = input('Continuar? S para si: ')
-print('Porcentaje de alumnos aprobados: ', (alumAprob*100)/contador)
-print('Promedio de los alumnos aprobados: ', (acumNotas/promAprob))
+print('Porcentaje de alumnos aprobados: ', (alumAprob*100)/contador,'%')
+print('Promedio de los alumnos aprobados: ', (notasAprob/promAprob))
+print('Promedio de los alumnos desaprobados: ', (notasDesaprob/promDesaprob))
+print('La nota mas alta de los aprobados es ', mayor)
